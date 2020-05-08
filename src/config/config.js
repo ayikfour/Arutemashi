496cb8803d9b22c4fce5a62789fdc4e53f1011d4
ayik four <ayikfour4@gmail.com>
2020-05-08 07:53:26 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -11,41 +11,41 @@ module.exports = {
    },
    twitter: {
       consumer_key:
-         process.env.TWITTER_CONSUMER_KEY || "0merVoVwnNLJ8Wq3mAwYLDudi",
+         process.env.TWITTER_CONSUMER_KEY || "TWITTER_CONSUMER_KEY",
       consumer_secret:
          process.env.TWITTER_CONSUMER_SECRET ||
-         "9WKEHaEoGaMKEHCluNXG5BO8GhAlbgvAzuDI6lGArYA4OuORsL",
+         "TWITTER_CONSUMER_SECRET",
       access_token:
          process.env.TWITTER_ACCESS_TOKEN ||
-         "1171122850549252096-IQcgpX5nEm2DLTXtHgsshEBCTJYREO",
+         "TWITTER_ACCESS_TOKEN",
       access_token_secret:
          process.env.TWITTER_ACCESS_TOKEN_SECRET ||
-         "GXvjW6U62BOcm54PxWfZffKoMvmTBQrUfGWpDnX0Dq6vw"
+         "TWITTER_ACCESS_TOKEN_SECRET"
    },
    unsplash: {
       access_key:
          process.env.UNSPLASH_ACCESS_KEY ||
-         "1a0ad8ac380e847b80bc381c82c6c07261e26f1375187465e099397a083167f6",
+         "UNSPLASH_ACCESS_KEY",
       secret_key:
          process.env.UNSPLASH_SECRET_KEY ||
-         "b007ef851362aed886f5304ce0afd6088ba8c834b5e6e94c77a7d4d515581280"
+         "UNSPLASH_SECRET_KEY"
    },
    tumblr: {
       consumer_key:
          process.env.TUMBLR_CONSUMER_KEY ||
-         "EzdMIbOg33ANNvG85sWwWD2T9lgRQh4tjJavAl1xDK0363Ku0b",
+         "TUMBLR_CONSUMER_KEY",
       consumer_secret:
          process.env.TUMBLR_CONSUMER_SECRET ||
-         "n6JoOsO6pqg0simtsIUbL66bz46okUiooZalTmHTESBUrfg8s9",
+         "TUMBLR_CONSUMER_SECRET",
       token:
          process.env.TUMBLR_TOKEN ||
-         "0gG7DPWlY0lpmCnNyhPS6GnHWeyfjXFU6TqkVbCxvRqxw5hKuf",
+         "TUMBLR_TOKEN",
       token_secret:
          process.env.TUMBLR_TOKEN_SECRET ||
-         "NcjBcSFEnKumrLvqSWAHODkxst2SbTwwU1jChYRrG8PXD7Q47C",
+         "TUMBLR_TOKEN_SECRET",
       api_key:
          process.env.TUMBLR_API_KEY ||
-         "EzdMIbOg33ANNvG85sWwWD2T9lgRQh4tjJavAl1xDK0363Ku0b"
+         "TUMBLR_API_KEY"
    },
    scheduler: {
       delete_photos: process.env.DELETE_PHOTOS || "0 */5 * * *",
